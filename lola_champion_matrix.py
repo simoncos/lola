a78be2151f97abbe76c00d2e17ae1a7796d59454
simoncos <l.z.simon@hotmail.com>
2016-03-17 12:53:49 +0800

    add lola_champion_rank.py

---
+++ lola_champion_matrix.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import sqlite3
+import time 
+
+addr_Database = 'lola_analysis_test.db'
+addr_Ori_Matrix = 'K-Matrix.csv'
+#addr_KillMatrix_output = 'C:/Users/Administrator/Desktop/KA_matrix/K-Matrix-test.csv'
+#addr_AssistMatrix_output = 'C:/Users/Administrator/Desktop/KA_matrix/A-Matrix-test.csv'
+
+df = pd.read_csv(addr_Ori_Matrix, sep=',', header=0, index_col=0)
+df = df.fillna(0)
+
+def main(): # can only generate one Matrix once a time
+	#start_time = time.time()
+	#extract_Kill_infor()
+	#end_time = time.time()
+	#matrix_to_csv(df, addr_KillMatrix_output)
+	start_time = time.time()
+	extract_Assist_infor()
+	end_time = time.time()
+	print(df)
+#	matrix_to_csv(df, addr_AssistMatrix_output)
+	print ('Generate Assist Matrix costs: %.2fs' %(end_time-start_time))
+
+def extract_Kill_infor():
+	temp_happen = []
+	conn = sqlite3.connect(addr_Database)
+	cursor = conn.execute('SELECT match_id,happen,victim,killer from FrameKillEvent')
+	i = 0
+	for row in cursor:
+		temp_happen.append(row[1])
+		if i==0:
+			temp_killer = row[3]
+			temp_victim = row[2]
+			df.ix[temp_killer, temp_victim] += 1
+			i += 1
+		else:
+			if not row[1]==temp_happen[i-1]:
+				temp_killer = row[3]
+				temp_victim = row[2]
+				df.ix[temp_killer, temp_victim] += 1
+			i += 1
+		#if i>=11:
+		#	break
+	
+#df.ix['Riven','Azir'] should be 2
+#df.ix['Riven','Lee Sin'] should be 1
+	conn.close()
+
+def extract_Assist_infor():
+	conn = sqlite3.connect(addr_Database)
+	cursor = conn.execute('SELECT killer,assist from FrameKillEvent') # consider the relationship between a&v or a&k?
+	#i = 0
+	for row in cursor:
+		if not row[1]==None:
+			temp_assist = row[1]
+			temp_killer = row[0]
+			df.ix[temp_killer, temp_assist] += 1
+		#i += 1
+		#if i>10:
+		#	break
+
+#df.ix['Ahri','Lee Sin'] should be 1
+#df.ix['Tristana','Nami'] should be 2
+	conn.close()
+
+def matrix_to_csv(df, output_addr):
+	df.to_csv(output_addr)
+
+
+	'''
+	for row in cursor:
+		if i<10:
+			print 'match_id', row[0]
+			print 'happen', row[1]
+			print 'victim', row[2]
+			print 'killer', row[3], '\n'
+			i += 1
+			# df.ix['Aatrox', 'Aatrox']
+	'''
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+"""
+def extract():
+	conn = sqlite3.connect('lola_null.db')
+	print 'connect 1'
+	conn.execute('''CREATE TABLE KA_MATRIX
+		(
+			match_id	text NOT NULL,
+			happen	int NOT NULL, 
+			kill_count	int NOT NULL,
+			assist_count	int NOT NULL,
+			champion_level	integer NOT NULL 
+		);''') 
+	print 'create table 1'
+	conn.close()
+"""
--- lola_champion_rank.py
+++ lola_champion_rank.py
@@ -4,6 +4,14 @@ LoLa data crawling based on Cassiopeia.
 """
 import pandas as pd
 from scipy.sparse import linalg
+import scipy.linalg
+import numpy 
+import networkx as nx
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
 
 def main():
 	champion_kill_rank()
@@ -12,19 +20,104 @@ def main():
 def champion_kill_rank(champion_kill_dataframe):
 	
 	champion_kill_matrix = champion_kill_dataframe.as_matrix()
+	row_sum = champion_kill_matrix.sum(axis=1)
+	champion_kill_matrix_norm = champion_kill_matrix / row_sum[:, numpy.newaxis] # broadcast
+
+	# Count
+
+	print("Champion Kill Rank by counts:")
+	countkills = pd.DataFrame()
+	countkills['champion'] = pd.Series(champion_kill_dataframe.index)
+	countkills['kill_count'] = champion_kill_matrix.sum(axis=1)
+	print_full(countkills.sort_values(by='kill_count', ascending=False))
+
+	print("Champion Death Rank by counts:")
+	countkills = pd.DataFrame()
+	countkills['champion'] = pd.Series(champion_kill_dataframe.index)
+	countkills['death_count'] = champion_kill_matrix.sum(axis=0)
+	print_full(countkills.sort_values(by='death_count', ascending=False))
+
+	# ED
 
 	print("Champion Kill Rank by eigenvector centralities without normalization:")
 	eigenkills = pd.DataFrame()
 	eigenkills['champion'] = pd.Series(champion_kill_dataframe.index)
-	eigenkills['kill_evector'] = pd.Series(abs(linalg.eigs(champion_kill_matrix, k=1)[1])) # eigenvector with largest eigenvalue (k=1); scipy >= 0.17.0
-	print(eigenkills.sort('kill_evector', ascending=False))
+	# eigenvector with largest eigenvalue (k=1), sometimes all negative, sometimes all positive, absolute values unchanged
+	eigenkills['kill_escore'] = pd.DataFrame(abs(linalg.eigs(champion_kill_matrix, k=1)[1]))
+	print_full(eigenkills.sort_values(by='kill_escore', ascending=False))
 
-	print("Champion Kill Rank by counts:")
-	countkills = pd.DataFrame(champion_kill_matrix.sum(1), columns=['champion', 'kill_count']
-	print(countkills.sort('kill_count', ascending=False))
+	print("Champion Kill Rank by eigenvector centralities normalized by kill counts:")
+	eigenkills_norm = pd.DataFrame()
+	eigenkills_norm['champion'] = pd.Series(champion_kill_dataframe.index)
+	eigenkills_norm['kill_escore_norm'] = pd.DataFrame(abs(linalg.eigs(champion_kill_matrix_norm, k=1)[1])) # eigenvector with largest eigenvalue (k=1); scipy >= 0.17.0
+	print_full(eigenkills_norm.sort_values(by='kill_escore_norm', ascending=False))
 
-def champion_assist_rank(champion_assist_matrix):
-	pass
+	print("Champion Death Rank by eigenvector centralities without normalization:")
+	eigendeaths = pd.DataFrame()
+	eigendeaths['champion'] = pd.Series(champion_kill_dataframe.index)
+	eigendeaths['death_escore'] = pd.DataFrame(abs(linalg.eigs(champion_kill_matrix.transpose(), k=1)[1]))
+	print_full(eigendeaths.sort_values(by='death_escore', ascending=False))
+
+	print("Champion Rank Kill/Death by eigenvector centralities")
+	eigenranks = pd.DataFrame()
+	eigenranks['champion'] = pd.Series(champion_kill_dataframe.index)
+	eigenranks['kill_escore'] = pd.DataFrame(abs(linalg.eigs(champion_kill_matrix, k=1)[1]))
+	eigenranks['death_escore'] = pd.DataFrame(abs(linalg.eigs(champion_kill_matrix.transpose(), k=1)[1]))
+	eigenranks['eranks'] = eigenranks['kill_escore'] / eigenranks['death_escore']
+	print_full(eigenranks.sort_values(by='eranks', ascending=False))	
+
+	# PageRank: similar result with eigenvector centrality
+
+	G = nx.to_networkx_graph(champion_kill_matrix)
+	G_transpose = nx.to_networkx_graph(champion_kill_matrix.transpose())
+
+	pr = nx.pagerank(G)
+	prkills = pd.DataFrame()
+	prkills['champion'] = pd.Series(champion_kill_dataframe.index)
+	prkills['kill_prscore'] = pd.DataFrame(data=list(pr.values()), index=list(pr.keys()))
+	print_full(prkills.sort_values(by='kill_prscore', ascending=False))	
+
+	pr_t = nx.pagerank(G_transpose)
+	prdeaths = pd.DataFrame()
+	prdeaths['champion'] = pd.Series(champion_kill_dataframe.index)
+	prdeaths['death_prscore'] = pd.DataFrame(data=list(pr_t.values()), index=list(pr_t.keys()))
+	print_full(prdeaths.sort_values(by='death_prscore', ascending=False))	
+
+	# HITS: hub=auth
+
+	hub, auth = nx.hits(G)
+	hitskills = pd.DataFrame()
+	hitskills['champion'] = pd.Series(champion_kill_dataframe.index)
+	hitskills['kill_hitsscore'] = pd.DataFrame(data=list(hub.values()), index=list(hub.keys()))
+	print_full(hitskills.sort_values(by='kill_hitsscore', ascending=False))	
+	hitsdeaths = pd.DataFrame()
+	hitsdeaths['champion'] = pd.Series(champion_kill_dataframe.index)
+	hitsdeaths['death_hitsscore'] = pd.DataFrame(data=list(auth.values()), index=list(auth.keys()))
+	print_full(hitsdeaths.sort_values(by='death_hitsscore', ascending=False))	
+
+def champion_assist_rank(champion_assist_dataframe):
+
+	champion_assist_matrix = champion_assist_dataframe.as_matrix()
+	row_sum = champion_assist_matrix.sum(axis=1)
+	champion_assist_matrix_norm = champion_assist_matrix / row_sum[:, numpy.newaxis] # broadcast
+
+	print("Champion assist Rank by counts:")
+	countassists = pd.DataFrame()
+	countassists['champion'] = pd.Series(champion_assist_dataframe.index)
+	countassists['assist_count'] = champion_assist_matrix.sum(axis=1)
+	print_full(countassists.sort_values(by='assist_count', ascending=False))
+
+	print("Champion assist Rank by eigenvector centralities without normalization:")
+	eigenassists = pd.DataFrame()
+	eigenassists['champion'] = pd.Series(champion_assist_dataframe.index)
+	eigenassists['assist_escore'] = pd.DataFrame(abs(linalg.eigs(champion_assist_matrix, k=1)[1]))
+	print_full(eigenassists.sort_values(by='assist_escore', ascending=False))
+
+	print("Champion assist Rank by eigenvector centralities normalized by assist counts:")
+	eigenassists_norm = pd.DataFrame()
+	eigenassists_norm['champion'] = pd.Series(champion_assist_dataframe.index)
+	eigenassists_norm['assist_escore_norm'] = pd.DataFrame(abs(linalg.eigs(champion_assist_matrix_norm, k=1)[1])) # eigenvector with largest eigenvalue (k=1); scipy >= 0.17.0
+	print_full(eigenassists_norm.sort_values(by='assist_escore_norm', ascending=False))
 
 if __name__ == '__main__':
 	main()
