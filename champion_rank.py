b73991609e5a8053e22f5e401473ac3c357ef7b2
simoncos <l.z.simon@hotmail.com>
2016-04-24 12:22:34 +0800

    tab to spaces(4), again

--- champion_rank.py
+++ champion_rank.py
@@ -10,114 +10,114 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def print_full(df): 
-	'''
-	print all rows of pd.DataFrame
-	'''
-	pd.set_option('display.max_rows', len(df))
-	print(df)
-	pd.reset_option('display.max_rows')
+    '''
+    print all rows of pd.DataFrame
+    '''
+    pd.set_option('display.max_rows', len(df))
+    print(df)
+    pd.reset_option('display.max_rows')
 
 def champion_win_rate_rank():
-	pass
+    pass
 
 def champion_pick_rate_rank():
     pass
 
 def champion_ban_rate_rank():
-	pass 
+    pass 
 
 def champion_average_kda_rank():
-	pass
+    pass
 
 def champion_distribution(champion_matrix_df, champion): #TODO
-	vector = champion_matrix_df.ix[champion]
-	values, base = np.histogram(vector, bins=40)
-	plt.plot(values)
+    vector = champion_matrix_df.ix[champion]
+    values, base = np.histogram(vector, bins=40)
+    plt.plot(values)
 
 def champion_cosine_similarity(champion_matrix_df, champion_1, champion_2): #TODO
-	return 1 - spatial.distance.cosine(champion_matrix_df.ix[champion_1], champion_matrix_df.ix[champion_2])
+    return 1 - spatial.distance.cosine(champion_matrix_df.ix[champion_1], champion_matrix_df.ix[champion_2])
 
 def champion_matrix_rank(champion_matrix_df, criteron, norm=False):
-	'''
-	champion_matrix_df: pd.DataFrame, kill/death/assist counts between champions, 
-						(a,b)=i means a kills / killed by / assists b for i times 
-	criteron: 'count', 'eigen', 'eigen_ratio', 'eigen_diff', 'pagerank', 'hits' 
-	norm: True, False
-	TODO: eigen_ratio and eigen_diff criteron can only be used in kill matrix, in assist do not make sense (would be forbidden in future)
-	'''
-	# must be float for linalg.eigs	
-	champion_matrix = champion_matrix_df.as_matrix().astype(float)
+    '''
+    champion_matrix_df: pd.DataFrame, kill/death/assist counts between champions, 
+                        (a,b)=i means a kills / killed by / assists b for i times 
+    criteron: 'count', 'eigen', 'eigen_ratio', 'eigen_diff', 'pagerank', 'hits' 
+    norm: True, False
+    TODO: eigen_ratio and eigen_diff criteron can only be used in kill matrix, in assist do not make sense (would be forbidden in future)
+    '''
+    # must be float for linalg.eigs 
+    champion_matrix = champion_matrix_df.as_matrix().astype(float)
 
-	if norm == 'row': # use row sum to normalize
-		row_sum = champion_matrix.sum(axis=1)
-		champion_matrix = champion_matrix / row_sum[:, np.newaxis] # pandas broadcast
-	
-	# Count
-	if criteron == 'count':
-		print("Champion Rank by counts:")
-		rank_df = pd.DataFrame()
-		rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		rank_df['count'] = champion_matrix.sum(axis=1)
-		print_full(rank_df.sort_values(by='count', ascending=False))
+    if norm == 'row': # use row sum to normalize
+        row_sum = champion_matrix.sum(axis=1)
+        champion_matrix = champion_matrix / row_sum[:, np.newaxis] # pandas broadcast
+    
+    # Count
+    if criteron == 'count':
+        print("Champion Rank by counts:")
+        rank_df = pd.DataFrame()
+        rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        rank_df['count'] = champion_matrix.sum(axis=1)
+        print_full(rank_df.sort_values(by='count', ascending=False))
 
-	# ED
-	elif criteron == 'eigen':
-		print("Champion Rank by eigenvector centrality")
-		rank_df = pd.DataFrame()
-		rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		# eigenvector with largest eigenvalue (k=1), sometimes all negative, sometimes all positive, absolute values unchanged
-		rank_df['eigen_1'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=1)[1]))
-		print_full(rank_df.sort_values(by='eigen_1', ascending=False))
+    # ED
+    elif criteron == 'eigen':
+        print("Champion Rank by eigenvector centrality")
+        rank_df = pd.DataFrame()
+        rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        # eigenvector with largest eigenvalue (k=1), sometimes all negative, sometimes all positive, absolute values unchanged
+        rank_df['eigen_1'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=1)[1]))
+        print_full(rank_df.sort_values(by='eigen_1', ascending=False))
 
-		#rank_df['eigen_2'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=2)[1][:,1]))		
-		#print_full(rank_df.sort_values(by='eigen_2', ascending=False))
+        #rank_df['eigen_2'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=2)[1][:,1]))      
+        #print_full(rank_df.sort_values(by='eigen_2', ascending=False))
 
-	# ED Ratio, eigen(M)/eigen(M.T)
-	elif criteron == 'eigen_ratio':
-		print("Champion Rank by eigenvector centrality ratio")
-		rank_df = pd.DataFrame()
-		rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		rank_df['eigen'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=1)[1]))
-		rank_df['eigen_t'] = pd.DataFrame(abs(linalg.eigs(champion_matrix.transpose(), k=1)[1]))
-		rank_df['eigen_ratio'] = rank_df['eigen'] / rank_df['eigen_t']
-		print_full(rank_df.sort_values(by='eigen_ratio', ascending=False))	
+    # ED Ratio, eigen(M)/eigen(M.T)
+    elif criteron == 'eigen_ratio':
+        print("Champion Rank by eigenvector centrality ratio")
+        rank_df = pd.DataFrame()
+        rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        rank_df['eigen'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=1)[1]))
+        rank_df['eigen_t'] = pd.DataFrame(abs(linalg.eigs(champion_matrix.transpose(), k=1)[1]))
+        rank_df['eigen_ratio'] = rank_df['eigen'] / rank_df['eigen_t']
+        print_full(rank_df.sort_values(by='eigen_ratio', ascending=False))  
 
-	# ED Diff, eigen(M)-eigen(M.T)
-	elif criteron == 'eigen_diff':
-		print("Champion Rank by eigenvector centrality difference")
-		rank_df = pd.DataFrame()
-		rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		rank_df['eigen'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=1)[1]))
-		rank_df['eigen_t'] = pd.DataFrame(abs(linalg.eigs(champion_matrix.transpose(), k=1)[1]))
-		rank_df['eigen_diff'] = rank_df['eigen'] - rank_df['eigen_t']
-		print_full(rank_df.sort_values(by='eigen_diff', ascending=False))	
+    # ED Diff, eigen(M)-eigen(M.T)
+    elif criteron == 'eigen_diff':
+        print("Champion Rank by eigenvector centrality difference")
+        rank_df = pd.DataFrame()
+        rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        rank_df['eigen'] = pd.DataFrame(abs(linalg.eigs(champion_matrix, k=1)[1]))
+        rank_df['eigen_t'] = pd.DataFrame(abs(linalg.eigs(champion_matrix.transpose(), k=1)[1]))
+        rank_df['eigen_diff'] = rank_df['eigen'] - rank_df['eigen_t']
+        print_full(rank_df.sort_values(by='eigen_diff', ascending=False))   
 
-	# PageRank: similar results with eigenvector centrality
-	elif criteron == 'pagerank':
-		print("Champion Rank by PageRank")
-		G = nx.DiGraph(champion_matrix)
-		pr = nx.pagerank(G)
-		rank_df = pd.DataFrame()
-		rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		rank_df['pagerank'] = pd.DataFrame(data=list(pr.values()), index=list(pr.keys()))
-		print_full(rank_df.sort_values(by='pagerank', ascending=False))	
+    # PageRank: similar results with eigenvector centrality
+    elif criteron == 'pagerank':
+        print("Champion Rank by PageRank")
+        G = nx.DiGraph(champion_matrix)
+        pr = nx.pagerank(G)
+        rank_df = pd.DataFrame()
+        rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        rank_df['pagerank'] = pd.DataFrame(data=list(pr.values()), index=list(pr.keys()))
+        print_full(rank_df.sort_values(by='pagerank', ascending=False)) 
 
-	# HITS:
-	elif criteron == 'hits':
-		print("Champion Rank by HITS")
-		G = nx.DiGraph(champion_matrix)
-		hub, auth = nx.hits(G)
-		hub_rank_df = pd.DataFrame()
-		hub_rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		hub_rank_df['hub'] = pd.DataFrame(data=list(hub.values()), index=list(hub.keys()))
-		print_full(hub_rank_df.sort_values(by='hub', ascending=False))	
-		auth_rank_df = pd.DataFrame()
-		auth_rank_df['champion'] = pd.Series(champion_matrix_df.index)
-		auth_rank_df['auth'] = pd.DataFrame(data=list(auth.values()), index=list(auth.keys()))
-		print_full(auth_rank_df.sort_values(by='auth', ascending=False))	
+    # HITS:
+    elif criteron == 'hits':
+        print("Champion Rank by HITS")
+        G = nx.DiGraph(champion_matrix)
+        hub, auth = nx.hits(G)
+        hub_rank_df = pd.DataFrame()
+        hub_rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        hub_rank_df['hub'] = pd.DataFrame(data=list(hub.values()), index=list(hub.keys()))
+        print_full(hub_rank_df.sort_values(by='hub', ascending=False))  
+        auth_rank_df = pd.DataFrame()
+        auth_rank_df['champion'] = pd.Series(champion_matrix_df.index)
+        auth_rank_df['auth'] = pd.DataFrame(data=list(auth.values()), index=list(auth.keys()))
+        print_full(auth_rank_df.sort_values(by='auth', ascending=False))    
 
-	else:
-		raise ValueError('Invalid criteron provided.')
+    else:
+        raise ValueError('Invalid criteron provided.')
 
 #TODO: pick, ban, win
 
@@ -125,50 +125,50 @@ def champion_matrix_rank(champion_matrix_df, criteron, norm=False):
 TODO: Visualization Part
 
 def top_banpick_win():
-	conn = sqlite3.connect('lola.db')
-	win_rate_matrix = []
-	pick_ban_infor = pd.read_sql("SELECT champion,picks,bans FROM ChampionMatchStats", conn, index_col=['champion'])
-	top_ten_pick_infor = pick_ban_infor.sort_values(by='picks', ascending=False).iloc[0:10]
-	top_ten_pick_champions = top_ten_pick_infor.index
-	for i in top_ten_pick_champions:
-		win = conn.execute("SELECT COUNT(champion) FROM Participant WHERE Participant.participant_win=1 and participant.champion=?",(i,))
-		win = win.fetchone()[0]
-		win_rate_matrix.append(float(win)/float(top_ten_pick_infor['picks'][i]))
-	top_ten_pick_infor['win_rate'] = win_rate_matrix
-	temp_series = pd.DataFrame(top_ten_pick_infor['win_rate']).sort_values(by='win_rate', ascending=True) # inverse the order
-	# top_ten_pick_infor = # add a new column then insert win_rate into it , then draw the image with name and win_rate
-	plt_pick = temp_series.plot(kind='barh', title='Win Rate of Pick Top 10 Champion', stacked=False).set_xlabel('Proportion').get_figure()
-	# plt_pick.savefig('win_rate_pick_Top_10.png')
-	
-	win_rate_matrix = []
-	top_ten_ban_infor = pick_ban_infor.sort_values(by='bans', ascending=False).iloc[0:10]
-	top_ten_ban_champions = top_ten_ban_infor.index
-	for i in top_ten_ban_champions:
-		win = conn.execute("SELECT COUNT(champion) FROM Participant WHERE Participant.participant_win=1 and participant.champion=?",(i,))
-		win = win.fetchone()[0]
-		win_rate_matrix.append(float(win)/float(top_ten_ban_infor['picks'][i]))
-	conn.close()
-	top_ten_ban_infor['win_rate'] = win_rate_matrix
-	temp_series = pd.DataFrame(top_ten_ban_infor['win_rate']).sort_values(by='win_rate', ascending=True) # inverse the order
-	#top_ten_pick_infor = # add a new column then insert win_rate into it , then draw the image with name and win_rate
-	plt_ban = temp_series.plot(kind='barh', title='Win Rate of Ban Top 10 Champion', stacked=False).set_xlabel('Proportion').get_figure()
-	# plt_ban.savefig('win_rate_ban_Top_10.png')
+    conn = sqlite3.connect('lola.db')
+    win_rate_matrix = []
+    pick_ban_infor = pd.read_sql("SELECT champion,picks,bans FROM ChampionMatchStats", conn, index_col=['champion'])
+    top_ten_pick_infor = pick_ban_infor.sort_values(by='picks', ascending=False).iloc[0:10]
+    top_ten_pick_champions = top_ten_pick_infor.index
+    for i in top_ten_pick_champions:
+        win = conn.execute("SELECT COUNT(champion) FROM Participant WHERE Participant.participant_win=1 and participant.champion=?",(i,))
+        win = win.fetchone()[0]
+        win_rate_matrix.append(float(win)/float(top_ten_pick_infor['picks'][i]))
+    top_ten_pick_infor['win_rate'] = win_rate_matrix
+    temp_series = pd.DataFrame(top_ten_pick_infor['win_rate']).sort_values(by='win_rate', ascending=True) # inverse the order
+    # top_ten_pick_infor = # add a new column then insert win_rate into it , then draw the image with name and win_rate
+    plt_pick = temp_series.plot(kind='barh', title='Win Rate of Pick Top 10 Champion', stacked=False).set_xlabel('Proportion').get_figure()
+    # plt_pick.savefig('win_rate_pick_Top_10.png')
+    
+    win_rate_matrix = []
+    top_ten_ban_infor = pick_ban_infor.sort_values(by='bans', ascending=False).iloc[0:10]
+    top_ten_ban_champions = top_ten_ban_infor.index
+    for i in top_ten_ban_champions:
+        win = conn.execute("SELECT COUNT(champion) FROM Participant WHERE Participant.participant_win=1 and participant.champion=?",(i,))
+        win = win.fetchone()[0]
+        win_rate_matrix.append(float(win)/float(top_ten_ban_infor['picks'][i]))
+    conn.close()
+    top_ten_ban_infor['win_rate'] = win_rate_matrix
+    temp_series = pd.DataFrame(top_ten_ban_infor['win_rate']).sort_values(by='win_rate', ascending=True) # inverse the order
+    #top_ten_pick_infor = # add a new column then insert win_rate into it , then draw the image with name and win_rate
+    plt_ban = temp_series.plot(kind='barh', title='Win Rate of Ban Top 10 Champion', stacked=False).set_xlabel('Proportion').get_figure()
+    # plt_ban.savefig('win_rate_ban_Top_10.png')
 
 
 def win_rate():
-	conn = sqlite3.connect('lola.db')
-	win_rate_matrix = []
-	pick_ban_infor = pd.read_sql("SELECT champion,picks,bans FROM ChampionMatchStats", conn, index_col=['champion'])
-	all_pick_infor = pick_ban_infor.sort_values(by='picks', ascending=False)
-	all_pick_champions = all_pick_infor.index
-	for i in all_pick_champions:
-		win = conn.execute("SELECT COUNT(champion) FROM Participant WHERE Participant.participant_win=1 and participant.champion=?",(i,))
-		win = win.fetchone()[0]
-		win_rate_matrix.append(float(win)/float(all_pick_infor['picks'][i]))
-	conn.close()
-	all_pick_infor['win_rate'] = win_rate_matrix
-	temp_series = pd.DataFrame(all_pick_infor['win_rate']).sort_values(by='win_rate', ascending=True)
-	# top_ten_pick_infor = # add a new column then insert win_rate into it , then draw the image with name and win_rate
-	plt_all = temp_series.plot(kind='barh', title='Win Rate Rank', stacked=False).set_xlabel('Proportion').get_figure()
-	# plt_all.savefig('win_rate.png')
+    conn = sqlite3.connect('lola.db')
+    win_rate_matrix = []
+    pick_ban_infor = pd.read_sql("SELECT champion,picks,bans FROM ChampionMatchStats", conn, index_col=['champion'])
+    all_pick_infor = pick_ban_infor.sort_values(by='picks', ascending=False)
+    all_pick_champions = all_pick_infor.index
+    for i in all_pick_champions:
+        win = conn.execute("SELECT COUNT(champion) FROM Participant WHERE Participant.participant_win=1 and participant.champion=?",(i,))
+        win = win.fetchone()[0]
+        win_rate_matrix.append(float(win)/float(all_pick_infor['picks'][i]))
+    conn.close()
+    all_pick_infor['win_rate'] = win_rate_matrix
+    temp_series = pd.DataFrame(all_pick_infor['win_rate']).sort_values(by='win_rate', ascending=True)
+    # top_ten_pick_infor = # add a new column then insert win_rate into it , then draw the image with name and win_rate
+    plt_all = temp_series.plot(kind='barh', title='Win Rate Rank', stacked=False).set_xlabel('Proportion').get_figure()
+    # plt_all.savefig('win_rate.png')
 '''
--- data_preprocess.py
+++ data_preprocess.py
@@ -9,42 +9,42 @@ Update MatchChampion:
 3. INSERT INTO MatchChampion VALUES(match_id, participant1, ...)
 '''
 def match_champion_to_sqlite():
-	conn = sqlite3.connect('lola.db')
-	cursor = conn.cursor()
+    conn = sqlite3.connect('lola.db')
+    cursor = conn.cursor()
 
-	print('Updating participants of matches to MatchChampion...')
+    print('Updating participants of matches to MatchChampion...')
 
-	# Select uncounted match_ids
-	cursor.execute('SELECT match_id FROM Match')
-	result = cursor.fetchall()
+    # Select uncounted match_ids
+    cursor.execute('SELECT match_id FROM Match')
+    result = cursor.fetchall()
 
-	st = time.time()
-	for item in result:
-	# Select participants of each match
-		match_id = int(item[0].encode('utf-8'))
-		cursor.execute('SELECT champion FROM Participant WHERE match_id = ? ORDER BY CAST(participant_id AS INTEGER)', (match_id,))
-		participants = cursor.fetchall()
-	# Insert participants into MatchChampion
-		cursor.execute('INSERT INTO MatchChampion VALUES(?,?,?,?,?,?,?,?,?,?,?)', (match_id, participants[0][0].encode('utf-8'),
-			participants[1][0].encode('utf-8'),participants[2][0].encode('utf-8'), participants[3][0].encode('utf-8'),
-			participants[4][0].encode('utf-8'),participants[5][0].encode('utf-8'), participants[6][0].encode('utf-8'),
-			participants[7][0].encode('utf-8'),participants[8][0].encode('utf-8'), participants[9][0].encode('utf-8'), ))
-		# print '%d Inserted.' % match_id
-	# Code for update
-	'''
-		cursor.execute('UPDATE MatchChampion SET participant1=?, participant2=?, participant3=?,\
-			participant4=?, participant5=?, participant6=?, participant7=?, participant8=?,participant9=?,\
-			participant10=? WHERE match_id = ?', (participants[0][0].encode('utf-8'), participants[1][0].encode('utf-8'),
-			participants[2][0].encode('utf-8'), participants[3][0].encode('utf-8'), participants[4][0].encode('utf-8'),
-			participants[5][0].encode('utf-8'), participants[6][0].encode('utf-8'), participants[7][0].encode('utf-8'),
-			participants[8][0].encode('utf-8'), participants[9][0].encode('utf-8'), item[0],))
-	'''
+    st = time.time()
+    for item in result:
+    # Select participants of each match
+        match_id = int(item[0].encode('utf-8'))
+        cursor.execute('SELECT champion FROM Participant WHERE match_id = ? ORDER BY CAST(participant_id AS INTEGER)', (match_id,))
+        participants = cursor.fetchall()
+    # Insert participants into MatchChampion
+        cursor.execute('INSERT INTO MatchChampion VALUES(?,?,?,?,?,?,?,?,?,?,?)', (match_id, participants[0][0].encode('utf-8'),
+            participants[1][0].encode('utf-8'),participants[2][0].encode('utf-8'), participants[3][0].encode('utf-8'),
+            participants[4][0].encode('utf-8'),participants[5][0].encode('utf-8'), participants[6][0].encode('utf-8'),
+            participants[7][0].encode('utf-8'),participants[8][0].encode('utf-8'), participants[9][0].encode('utf-8'), ))
+        # print '%d Inserted.' % match_id
+    # Code for update
+    '''
+        cursor.execute('UPDATE MatchChampion SET participant1=?, participant2=?, participant3=?,\
+            participant4=?, participant5=?, participant6=?, participant7=?, participant8=?,participant9=?,\
+            participant10=? WHERE match_id = ?', (participants[0][0].encode('utf-8'), participants[1][0].encode('utf-8'),
+            participants[2][0].encode('utf-8'), participants[3][0].encode('utf-8'), participants[4][0].encode('utf-8'),
+            participants[5][0].encode('utf-8'), participants[6][0].encode('utf-8'), participants[7][0].encode('utf-8'),
+            participants[8][0].encode('utf-8'), participants[9][0].encode('utf-8'), item[0],))
+    '''
 
-	print('Done.\nElapsed time: %.2fs.\n' % (time.time()-st))
+    print('Done.\nElapsed time: %.2fs.\n' % (time.time()-st))
 
-	cursor.close()
-	conn.commit()
-	conn.close()
+    cursor.close()
+    conn.commit()
+    conn.close()
 
 '''
 Update ChampionMatchStats' kdas and damages:
@@ -53,87 +53,87 @@ Update ChampionMatchStats' kdas and damages:
 3. UPDATE ChampionMatchStats SET picks = ?, bans = ? WHERE champion = ?
 '''
 def champion_match_stats_to_sqlite():
-	# TODO: champion dynamic enumeration using select distinct(champion) from participant
-	conn = sqlite3.connect('lola.db')
-	cursor = conn.cursor()
+    # TODO: champion dynamic enumeration using select distinct(champion) from participant
+    conn = sqlite3.connect('lola.db')
+    cursor = conn.cursor()
 
-	print('Updating champion stats of matches to ChampionMatchStats...')
-	# Select kda, damages, wards... of every champions
-	st = time.time()
-	for champion in champions:
-		cursor.execute('SELECT champion FROM ChampionMatchStats WHERE champion = ?', (champion,))
-		exist = cursor.fetchone()
-		if exist is None:
-			print(champion)
-			cursor.execute('SELECT sum(kills), sum(deaths), sum(assists), sum(gold_earned), sum(magic_damage_dealt_to_champions),\
-				sum(physical_damage_dealt_to_champions), sum(true_damage_dealt_to_champions), sum(damage_taken),\
-				sum(crowd_control_dealt), sum(ward_kills), sum(wards_placed) FROM Participant WHERE champion = ?', (champion, ))
-			result = cursor.fetchone()
-			# Insert part of champion stats, excluding picks/bans/wins and stats of team
-			cursor.execute('INSERT INTO ChampionMatchStats VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (champion,
-				0, 0, 0, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
-				result[10], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, None, None))
-			# print champion, '\n', result
-		# Update picks/bans/wins
-		cursor.execute('SELECT COUNT(champion) FROM Participant WHERE champion = ?',(champion,))
-		picks = cursor.fetchone()[0]
-		cursor.execute('SELECT COUNT(ban) FROM TeamBan WHERE ban = ?',(champion,))
-		bans = cursor.fetchone()[0]
-		cursor.execute('SELECT SUM(participant_win) FROM Participant WHERE champion = ?',(champion,))		
-		wins = cursor.fetchone()[0]		
-		cursor.execute('UPDATE ChampionMatchStats SET picks = ?, bans = ?, wins = ? WHERE champion = ?',(picks, bans, wins, champion,))
+    print('Updating champion stats of matches to ChampionMatchStats...')
+    # Select kda, damages, wards... of every champions
+    st = time.time()
+    for champion in champions:
+        cursor.execute('SELECT champion FROM ChampionMatchStats WHERE champion = ?', (champion,))
+        exist = cursor.fetchone()
+        if exist is None:
+            print(champion)
+            cursor.execute('SELECT sum(kills), sum(deaths), sum(assists), sum(gold_earned), sum(magic_damage_dealt_to_champions),\
+                sum(physical_damage_dealt_to_champions), sum(true_damage_dealt_to_champions), sum(damage_taken),\
+                sum(crowd_control_dealt), sum(ward_kills), sum(wards_placed) FROM Participant WHERE champion = ?', (champion, ))
+            result = cursor.fetchone()
+            # Insert part of champion stats, excluding picks/bans/wins and stats of team
+            cursor.execute('INSERT INTO ChampionMatchStats VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (champion,
+                0, 0, 0, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
+                result[10], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, None, None))
+            # print champion, '\n', result
+        # Update picks/bans/wins
+        cursor.execute('SELECT COUNT(champion) FROM Participant WHERE champion = ?',(champion,))
+        picks = cursor.fetchone()[0]
+        cursor.execute('SELECT COUNT(ban) FROM TeamBan WHERE ban = ?',(champion,))
+        bans = cursor.fetchone()[0]
+        cursor.execute('SELECT SUM(participant_win) FROM Participant WHERE champion = ?',(champion,))       
+        wins = cursor.fetchone()[0]     
+        cursor.execute('UPDATE ChampionMatchStats SET picks = ?, bans = ?, wins = ? WHERE champion = ?',(picks, bans, wins, champion,))
 
-	print('Done.\nElapsed time: %.2fs\n' % (time.time() - st))
+    print('Done.\nElapsed time: %.2fs\n' % (time.time() - st))
 
-	cursor.close()
-	conn.commit()
-	conn.close()
+    cursor.close()
+    conn.commit()
+    conn.close()
 
 # TODO: ChampionMatchStats and ChampionRank Initialization (insert or update), in case that champion / match data updating from crawling
 
 '''TODO: average tier of match
 def select_version_tier():
-	conn = sqlite3.connect(addr_db)
-	cursor = conn.execute("SELECT match_id,version from Match")
-	for row in cursor:
-		all_matchid.append(row[0])
-		all_version.append(row[1])
-		if row[1] not in version:
-			version.append(row[1])
-	
-	cursor = conn.execute("SELECT previous_season_tier from Participant")
-	count = 1
-	temp_avg_tier = []
-	for row in cursor:
-		if row[0] not in tier: # collect all tier
-			tier.append(row[0])
-		if count%10!=0: # collect match tier level
-			temp_avg_tier.append(row[0])
-			count += 1
-		else:
-			avg_tier.append(most_common(temp_avg_tier))
-			count = 1
-			temp_avg_tier = []
-	conn.close
-	for i in range(len(all_matchid)):
-		all_matchid[i] = all_matchid[i].encode("ascII")
-		avg_tier[i] = avg_tier[i].encode("ascII")
+    conn = sqlite3.connect(addr_db)
+    cursor = conn.execute("SELECT match_id,version from Match")
+    for row in cursor:
+        all_matchid.append(row[0])
+        all_version.append(row[1])
+        if row[1] not in version:
+            version.append(row[1])
+    
+    cursor = conn.execute("SELECT previous_season_tier from Participant")
+    count = 1
+    temp_avg_tier = []
+    for row in cursor:
+        if row[0] not in tier: # collect all tier
+            tier.append(row[0])
+        if count%10!=0: # collect match tier level
+            temp_avg_tier.append(row[0])
+            count += 1
+        else:
+            avg_tier.append(most_common(temp_avg_tier))
+            count = 1
+            temp_avg_tier = []
+    conn.close
+    for i in range(len(all_matchid)):
+        all_matchid[i] = all_matchid[i].encode("ascII")
+        avg_tier[i] = avg_tier[i].encode("ascII")
 
 def insert_avgtier():
-	conn = sqlite3.connect(addr_db)
-	conn.execute("ALTER TABLE Match ADD COLUMN TIER TEXT") # Add COLUMN in Match(#should be dropped)
-	for i in range(len(avg_tier)): # insert the average match tier
-		conn.execute("UPDATE Match SET TIER=? WHERE match_id=?",(avg_tier[i],all_matchid[i]))
-	conn.commit()
-	print '$-----Table:Match Mission:avg_tier update [Finished].-----$'	
-	conn.execute("ALTER TABLE FrameKillEvent ADD COLUMN avg_tier TEXT") # Add COLUMN in Frame(#should be dropped)
-	conn.execute("ALTER TABLE FrameKillEvent ADD COLUMN version TEXT") # Add COLUMN in Frame(#should be dropped)
-	conn.execute("UPDATE FrameKillEvent SET avg_tier = (SELECT TIER FROM Match WHERE Match.match_id = FrameKillEvent.match_id)")
-	conn.execute("UPDATE FrameKillEvent SET version = (SELECT version FROM Match WHERE Match.match_id = FrameKillEvent.match_id)")
-	print '$-----Table:FrameKillEvent Mission:avg_tier&version update [Finished].-----$'
-	conn.commit()
-	conn.close()
+    conn = sqlite3.connect(addr_db)
+    conn.execute("ALTER TABLE Match ADD COLUMN TIER TEXT") # Add COLUMN in Match(#should be dropped)
+    for i in range(len(avg_tier)): # insert the average match tier
+        conn.execute("UPDATE Match SET TIER=? WHERE match_id=?",(avg_tier[i],all_matchid[i]))
+    conn.commit()
+    print '$-----Table:Match Mission:avg_tier update [Finished].-----$' 
+    conn.execute("ALTER TABLE FrameKillEvent ADD COLUMN avg_tier TEXT") # Add COLUMN in Frame(#should be dropped)
+    conn.execute("ALTER TABLE FrameKillEvent ADD COLUMN version TEXT") # Add COLUMN in Frame(#should be dropped)
+    conn.execute("UPDATE FrameKillEvent SET avg_tier = (SELECT TIER FROM Match WHERE Match.match_id = FrameKillEvent.match_id)")
+    conn.execute("UPDATE FrameKillEvent SET version = (SELECT version FROM Match WHERE Match.match_id = FrameKillEvent.match_id)")
+    print '$-----Table:FrameKillEvent Mission:avg_tier&version update [Finished].-----$'
+    conn.commit()
+    conn.close()
 
 def most_common(L):
-	return max(g(sorted(L)), key=lambda(x, v):(len(list(v)),-L.index(x)))[0]
+    return max(g(sorted(L)), key=lambda(x, v):(len(list(v)),-L.index(x)))[0]
 '''
